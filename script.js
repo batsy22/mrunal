f0489da420c24ed9c70f4506f73a2964ea87fa97
batsy22 <pman22332@gmail.com>
2025-02-27 19:29:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("loveButton").addEventListener("click", function() {
+    const loveMessage = document.getElementById("loveMessage");
+    loveMessage.style.display = "block";
+});
